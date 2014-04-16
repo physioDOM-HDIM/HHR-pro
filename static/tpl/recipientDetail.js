34d8ad874572497a8e0d63a37d649ba7612eeec3
fablec <fabrice.lecoz@telecomsante.com>
2014-04-16 06:38:49 +0200

    Enhance display

--- static/tpl/recipientDetail.js
+++ static/tpl/recipientDetail.js
@@ -2,5 +2,7 @@ window.addEventListener("DOMContentLoaded",init,false);
 
 function init() {
 	setFixedTable("#stakeholders");
+	setFixedTable("#personalContact");
 	document.querySelector("#collapse2").classList.remove("in");
+	document.querySelector("#collapse3").classList.remove("in");
 }
