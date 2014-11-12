016b057f5e742231961e083de66e816e274243bb
fablec <fabrice.lecoz@telecomsante.com>
2014-11-12 10:55:10 +0100

    IPage : factor methods

--- server/lib/http/IPage.js
+++ server/lib/http/IPage.js
@@ -33,54 +33,34 @@ function IPage() {
 		var html;
 		
 		init(req);
-		/*
-		i18n.setLocale(req.cookies.lang || req.params.lang || "en");
-
-		swig.setDefaults({cache: false});
-		swig.setFilter('i18n', function (input, idx) {
-			console.log("input", input, idx);
-			return i18n.__(input);
-		});
-		*/
 		
 		html = swig.renderFile('./static/tpl/beneficiaryCreate.htm');
-		// html = swig.renderFile('./static/tpl/beneficiaries.htm');
-		
-		res.writeHead(200, {
-			'Content-Length': Buffer.byteLength(html),
-			'Content-Type'  : 'text/html'
-		});
-		res.write(html);
-		res.end();
-		next();
+		sendPage(html,res, next);
 	};
 
 	this.beneficiarySelect = function(req, res, next) {
 		logger.trace("beneficiarySelect");
 		var html;
 		
-		i18n.setLocale(req.cookies.lang || req.params.lang || "fr");
-		
-		swig.setDefaults({cache: false});
-		swig.setFilter('i18n', function (input, idx) {
-			console.log("input", input, idx);
-			return i18n.__(input);
-		});
-		
 		var data = { 
 			admin: ["coordinator","administrator"].indexOf(req.session.role) !== -1?true:false 
-		}
+		};
 		html = swig.renderFile('./static/tpl/beneficiaries.htm', data);
 		
+		sendPage(html,res, next);
+	};
+	
+	function sendPage( html, res, next ) {
+		logger.trace("sendPage");
 		res.writeHead(200, {
 			'Content-Length': Buffer.byteLength(html),
 			'Content-Type'  : 'text/html'
 		});
-		
+
 		res.write(html);
 		res.end();
 		next();
-	};
+	}
 }
 
 module.exports = new IPage();
