6f05c7d1d6467e81ce8b18005de0c802d4ddd505
fablec <fabrice.lecoz@telecomsante.com>
2014-11-14 08:42:10 +0100

    Save locales

---
+++ locales/fr.js
@@ -0,0 +1,33 @@
+{
+	"Hello": "Bonjour",
+	"%s cat": {
+		"one": "%s chat",
+		"other": "%s chats"
+	},
+	"tree": "arbre",
+	"There is one monkey in the %%s": {
+		"one": "il y a un singe dans l'%%s",
+		"other": "il y a %d singes dans l'%%s"
+	},
+	"Gender": "Genre",
+	"Male": "Homme",
+	"Female": "Femme",
+	"Last name": "Nom",
+	"First name": "Prénom",
+	"Birth date": "Date de naissance",
+	"Perimeter": "Périmètre",
+	"Height (meters)": "Taille (mètre)",
+	"Social ID": "N° INSEE",
+	"Phone": "Téléphone",
+	"Mobile": "Mobile",
+	"Street": "Rue",
+	"Zip code": "Code postal",
+	"City": "Ville",
+	"Add": "Ajout",
+	"Save": "Enregistrer",
+	"Cancel": "Annuler",
+	"Validate": "Valider",
+	"Select beneficiary": "Selection d'un bénéficiaire",
+	"Last Name": "Last Name",
+	"Zip Code": "Zip Code"
+}
