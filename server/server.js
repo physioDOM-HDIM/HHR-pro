b1d75d4dc696889dd8b6e5153d7b7ed513319280
fablec <fabrice.lecoz@telecomsante.com>
2014-11-16 17:21:08 +0100

    Directory manage directory create url

--- server/server.js
+++ server/server.js
@@ -301,6 +301,7 @@ server.get( '/logout', logout);
 server.get( '/beneficiary/create', IPage.beneficiaryCreate);
 server.get( '/beneficiary/select', IPage.beneficiarySelect);
 server.get( '/directory', IPage.directoryList);
+server.get( '/directory/create', IPage.directoryUpdate);
 server.get( '/directory/:professionalID', IPage.directoryUpdate);
 
 server.get(/\/[^api\/]?$/, function(req, res, next) {
