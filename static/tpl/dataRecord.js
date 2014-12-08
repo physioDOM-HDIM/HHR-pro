6c46b213c9eff69eb0b38676c0a68ed4722a8ee2
Antoine Le Chapelain <alechapelain@rennes.sii.fr>
2014-12-08 17:27:59 +0100

    [DataRecord] Edit/Add item in UI + fixes on view

--- static/tpl/dataRecord.js
+++ static/tpl/dataRecord.js
@@ -1,91 +1,76 @@
 'use strict';
-
 //Get the base url of the tsante-list component (without potential params)
 function getBaseURL(url) {
-	console.log("getBaseURL", arguments);
-	var idx = url.indexOf("?");
-	return url.slice(0, idx !== -1 ? idx : url.length);
+    console.log("getBaseURL", arguments);
+    var idx = url.indexOf("?");
+    return url.slice(0, idx !== -1 ? idx : url.length);
 }
 
 function paginate(init, params) {
-	var listPagerElt = document.querySelector("tsante-list");
-
-	if (params) {
-		listPagerElt.url = getBaseURL(listPagerElt.url) + params;
-	} else {
-		listPagerElt.url = getBaseURL(listPagerElt.url);
-	}
-
-	if (init) {
-		listPagerElt.pg = 1;
-	}
-
-	console.log("url: ", listPagerElt.url);
-	listPagerElt.go();
+    var listPagerElt = document.querySelector("tsante-list");
+    if (params) {
+        listPagerElt.url = getBaseURL(listPagerElt.url) + params;
+    } else {
+        listPagerElt.url = getBaseURL(listPagerElt.url);
+    }
+    if (init) {
+        listPagerElt.pg = 1;
+    }
+    console.log("url: ", listPagerElt.url);
+    listPagerElt.go();
 }
 
 function getParams() {
-
-	var filterForm = document.forms.filter,
-		orderForm = document.forms.order,
-		objFilter = form2js(filterForm),
-		objOrder = form2js(orderForm),
-		params = "";
-		
-console.log(objFilter);
-
-
-	if (JSON.stringify(objFilter) !== "{}") {
-		params += "?filter=" + JSON.stringify(objFilter);
-	}
-	if (objOrder.sort) {
-		params += (params ? "&" : "?") + "sort=" + objOrder.sort;
-	}
-	if (objOrder.dir) {
-		params += (params ? "&" : "?") + "dir=" + objOrder.dir;
-	}
-
-	return params;
+    var filterForm = document.forms.filter,
+        orderForm = document.forms.order,
+        objFilter = form2js(filterForm),
+        objOrder = form2js(orderForm),
+        params = "";
+    console.log(objFilter);
+    if (JSON.stringify(objFilter) !== "{}") {
+        params += "?filter=" + JSON.stringify(objFilter);
+    }
+    if (objOrder.sort) {
+        params += (params ? "&" : "?") + "sort=" + objOrder.sort;
+    }
+    if (objOrder.dir) {
+        params += (params ? "&" : "?") + "dir=" + objOrder.dir;
+    }
+    return params;
 }
 
 function resetOrder() {
-	document.forms.order.reset();
-	var params = getParams();
-	paginate(false, params);
+    document.forms.order.reset();
+    var params = getParams();
+    paginate(false, params);
 }
 
 function resetFilter() {
-	document.forms.filter.reset();
-	var startDateContainer = document.querySelector('.startDate'),
-		endDateContainer = document.querySelector('.endDate');
-
-	startDateContainer.value = '';
-	endDateContainer.value = '';
-
-	console.log(endDateContainer);
-
-	var params = getParams();
-	paginate(false, params);
+    document.forms.filter.reset();
+    var startDateContainer = document.querySelector('.startDate'),
+        endDateContainer = document.querySelector('.endDate');
+    startDateContainer.value = '';
+    endDateContainer.value = '';
+    console.log(endDateContainer);
+    var params = getParams();
+    paginate(false, params);
 }
 
 function validFilter() {
-	var params = getParams();
-	paginate(true, params);
+    var params = getParams();
+    paginate(true, params);
 }
+
 function init() {
-	var list = document.querySelector('tsante-list');
-
-	list.addEventListener('tsante-response', function(datas) {
-		var i = 0, 
-			len = datas.detail.list.items.length;
-
-		for(i; i<len; i++) {
-			datas.detail.list.items[i].date = moment.utc(datas.detail.list.items[i].datetime).format("YYYY-MM-DD | HH:mm");
-		}
-
-		list.render(datas.detail);
-
-	});
+    var list = document.querySelector('tsante-list');
+    list.addEventListener('tsante-response', function(datas) {
+        var i = 0,
+            len = datas.detail.list.items.length;
+        for (i; i < len; i++) {
+            datas.detail.list.items[i].date = moment.utc(datas.detail.list.items[i].datetime).format("YYYY-MM-DD | HH:mm");
+        }
+        list.render(datas.detail);
+    });
 }
-
 window.addEventListener("polymer-ready", init, false);
+
--- static/tpl/dataRecordEdit.js
+++ static/tpl/dataRecordEdit.js
@@ -1,17 +1,54 @@
+"use strict";
+
 window.addEventListener("DOMContentLoaded", function() {
     var elt = document.querySelector("#datetime").innerText,
         modelData = {
             dateFormat: function() {
                 return function(val, render) {
-                    console.log(render(val))
                     return moment.utc(render(val)).format("YYYY-MM-DD | HH:mm") + ' ';
-                }
+                };
             }
         };
 
      document.querySelector("#datetime").innerText = Mustache.render(elt, modelData);
 }, false);
 
+
+/* UI Actions */
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+function toggleEditMode(id) {
+    var line = document.querySelector('#ID' + id),
+        updateMode = line.querySelector('.updateMode'),
+        readMode = line.querySelector('.readMode');
+
+    if (hasClass(updateMode, 'hidden')) {
+        updateMode.className = 'updateMode';
+        readMode.className = 'readMode hidden';
+    } else {
+        updateMode.className = 'updateMode hidden';
+        readMode.className = 'readMode';
+    }
+}
+
+function addLine(category) {
+    var tpl = document.querySelector('#newItem').innerHTML,
+        container = document.querySelector('#newItems-'+category);
+
+    container.innerHTML += tpl;
+}
+
+function removeLine(element) {
+    var line = element.parentNode.parentNode,
+        container = line.parentNode;
+
+    container.removeChild(line);
+}
+
+
+/* Form Valid (TODO) */
 var promiseXHR = function(method, url, statusOK, data) {
     var promise = new RSVP.Promise(function(resolve, reject) {
         var client = new XMLHttpRequest();
@@ -32,75 +69,12 @@ var promiseXHR = function(method, url, statusOK, data) {
     return promise;
 };
 
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-}
-
-function toggleEditMode(id) {
-    var line = document.querySelector('#ID' + id),
-        updateMode = line.querySelector('.updateMode'),
-        readMode = line.querySelector('.readMode');
-
-    if (hasClass(updateMode, 'hidden')) {
-        updateMode.className = "updateMode";
-        readMode.className = "readMode hidden";
-    } else {
-        updateMode.className = "updateMode hidden";
-        readMode.className = "readMode";
-    }
-}
-
-function toggleEditMode(id) {
-    var line = document.querySelector('#ID' + id),
-        updateMode = line.querySelector('.updateMode'),
-        readMode = line.querySelector('.readMode');
-
-    if (hasClass(updateMode, 'hidden')) {
-        updateMode.className = "updateMode";
-        readMode.className = "readMode hidden";
-    } else {
-        updateMode.className = "updateMode hidden";
-        readMode.className = "readMode";
-    }
-}
-
-function toggleAddMode(category) {
-    var line = document.querySelector('#add' + category),
-        addModeOn = line.querySelector('.addModeOn'),
-        addModeOff = line.querySelector('.addModeOff');
-
-    if (hasClass(addModeOn, 'hidden')) {
-        addModeOn.className = "addModeOn";
-        addModeOff.className = "addModeOff hidden";
-    } else {
-        addModeOn.className = "addModeOn hidden";
-        addModeOff.className = "addModeOff";
-    }
-}
-
-
-function updateMode(id) {
-    toggleEditMode(id);
-}
-
-function addMode(category) {
-    toggleAddMode(category);
-}
-
-
 function updateMinMax(obj) {
     console.log("updateDataRecordItems", obj);
     //TODO When data model for min max threshold is written
 }
 
-function updateItem(id) {
-    //TODO Update Logic for item
-    //then end with: 
-    toggleEditMode(id);
-}
-
-function addItem(category) {
-    //TODO Add Logic for new Item
-    //then end with: 
-    toggleAddMode(category);
+function save() {
+    //TODO
+    window.alert('afficher modal confirmation puis envoyer données');
 }
