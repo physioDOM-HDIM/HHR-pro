430ef527656e11d4636363f255ac7c7b33371ce4
jledentu <jledentu@gmail.com>
2014-12-10 18:19:59 +0100

    Display service info when clicking on the agenda

--- static/tpl/agenda.js
+++ static/tpl/agenda.js
@@ -1,3 +1,5 @@
+/*global RSVP:false */
+
 'use strict';
 
 var promiseXHR = function(method, url, statusOK, data) {
@@ -23,10 +25,84 @@ var promiseXHR = function(method, url, statusOK, data) {
 
 document.addEventListener('polymer-ready', function() {
 
+	// Load event data
+	promiseXHR('GET', '/events.json', 200)
+	.then(function(response) {
+			document.getElementById('agenda').events = JSON.parse(response);
+		},
+		function(error) {
+			console.log(error);
+		});
+
 	// Handle clicks on event items
 	document.getElementById('agenda').addEventListener('zdk-event-click', function(e) {
-		// Load event data
-		promiseXHR("GET", "/api/", 200)
+
+		var data = e.detail.event;
+
+		// TODO: remove with REST calls
+
+		var service = {};
+		service.ref = data.label;
+		service.startDate = moment('2014-12-15', 'YYYY-MM-DD').format('L');
+		service.endDate = moment('2014-12-31', 'YYYY-MM-DD').format('L');
+		service.provider = 'CHAZAL C.';
+
+		switch (data.serviceID) {
+			case 1:
+				service.detail = 'Description of the care to make';
+				service.frequency = 'weekly';
+				service.when = [
+					{
+						day: 'monday',
+						time: '9:00'
+					},
+					{
+						day: 'wednesday',
+						time: '9:00'
+					},
+					{
+						day: 'friday',
+						time: '9:00'
+					},
+				];
+				break;
+			case 2:
+				service.detail = 'Description of the assistance';
+				service.frequency = 'daily';
+				service.weekEnd = true;
+				service.when = {time: '12:00'};
+				break;
+			case 3:
+				service.detail = 'Description of the service';
+				service.frequency = 'weekly';
+				service.when = [
+					{
+						day: 'tuesday',
+						time: '18:00'
+					},
+					{
+						day: 'thursday',
+						time: '18:00'
+					},
+					{
+						day: 'saturday',
+						time: '18:00'
+					},
+				];
+				break;
+		}
+
+		service.isDaily = service.frequency === 'daily';
+
+		var dialogEventTemplate = document.getElementById('dialog-event-template').innerHTML;
+		Mustache.parse(dialogEventTemplate);
+		var rendered = Mustache.render(dialogEventTemplate, {service: service});
+		document.getElementById('dialog-event').innerHTML = rendered;
+
+		var eventDialog = document.getElementById('dialog-event');
+		eventDialog.show();
+
+		/*promiseXHR("GET", "/api/", 200)
 			.then(function(response) {
 
 				var e = response;
@@ -41,6 +117,6 @@ document.addEventListener('polymer-ready', function() {
 			},
 			function(error) {
 				
-			});
+			});*/
 	});
 });
