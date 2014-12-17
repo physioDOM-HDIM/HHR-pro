69bd005248d3330fc217752f581d80280863eb83
Antoine Le Chapelain <alechapelain@rennes.sii.fr>
2014-12-17 15:09:56 +0100

    [PrescriptionData] View + init modal

---
+++ static/tpl/prescriptionData.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var openModalEdition = function() {
+    showModal();
+}
+
+
+
+/* Modal */
+function closeModal() {
+    document.querySelector("#editModal").hide();
+}
+
+
+function showModal() {
+    document.querySelector("#editModal").show();
+}
