1b4f1d72e4a5605cae9cd3d6c99d3b8417b628ca
fablec <fabrice.lecoz@telecomsante.com>
2015-01-23 21:00:27 +0100

    FIX Beneficiaries filter

--- static/tpl/beneficiaries.js
+++ static/tpl/beneficiaries.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //Filter data format
 var dataFormat = {
 		nameData: "name.family",
@@ -15,36 +17,59 @@ function getBaseURL(url) {
 	return url.slice(0, idx !== -1 ? idx : url.length);
 }
 
-function resetFilter() {
-	console.log("resetFilter", arguments);
-	document.forms.filter.reset();
+function paginate(init, params) {
 	var listPagerElt = document.querySelector("tsante-list");
-	listPagerElt.url = getBaseURL(listPagerElt.url);
+
+	if (params) {
+		listPagerElt.url = getBaseURL(listPagerElt.url) + params;
+	} else {
+		listPagerElt.url = getBaseURL(listPagerElt.url);
+	}
+	if (init) {
+		listPagerElt.pg = 1;
+	}
+
 	listPagerElt.go();
 }
 
-function validFilter() {
-	console.log("validFilter", arguments);
+function getParams() {
 	var filterForm = document.forms.filter,
-		listPagerElt = document.querySelector("tsante-list"),
-		params = "",
-		nameValue = "",
-		filterObj = {},
-		obj = form2js(filterForm);
-	
-	if(JSON.stringify(obj) !== "{}"){
-		params += "?filter=" + JSON.stringify(obj);
-	}
-	if(obj.sort) {
-		params += (params?"&":"?")+"sort="+obj.sort;
-	}
-	if(obj.dir) {
-		params += (params?"&":"?")+"dir="+obj.dir;
-	}
-	listPagerElt.url = getBaseURL(listPagerElt.url) + params;
+		orderForm = document.forms.order,
+		objFilter = form2js(filterForm),
+		objOrder = form2js(orderForm),
+		params = "";
 
-	console.log("validFilter - url: ", listPagerElt.url);
+	if( objFilter.perimeter === "NONE" ) {
+		delete objFilter.perimeter;
+	}
+	if (JSON.stringify(objFilter) !== "{}") {
+		params += "?filter=" + JSON.stringify(objFilter);
+	}
+	if (objOrder.sort) {
+		params += (params ? "&" : "?") + "sort=" + objOrder.sort;
+	}
+	if (objOrder.dir) {
+		params += (params ? "&" : "?") + "dir=" + objOrder.dir;
+	}
+	return params;
+}
 
-	listPagerElt.pg = 1;
-	listPagerElt.go();
+function resetFilter() {
+	document.forms.filter.reset();
+	document.querySelector('.startDate').value = '';
+	document.querySelector('.stopDate').value = '';
+
+	var params = getParams();
+	paginate(false, params);
+}
+
+function resetOrder() {
+	document.forms.order.reset();
+	var params = getParams();
+	paginate(false, params);
+}
+
+function validFilter() {
+	var params = getParams();
+	paginate(true, params);
 }
