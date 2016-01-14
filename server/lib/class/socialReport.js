42ebd4fe72b5c564e3a2c08fa70dfde63a51c2f5
fablec <fabrice.lecoz@telecomsante.com>
2016-01-14 21:13:34 +0100

    A first version of social report , tbc ...

---
+++ server/lib/class/socialReport.js
@@ -0,0 +1,59 @@
+/**
+ * @file socialReport.js
+ */
+
+/* jslint node:true */
+/* global physioDOM */
+"use strict";
+
+var RSVP = require("rsvp"),
+	dbPromise = require("./database.js"),
+	promise = RSVP.Promise,
+	Logger = require("logger"),
+	ObjectID = require("mongodb").ObjectID,
+	messageSchema = require("./../schema/messageSchema"),
+	moment = require("moment");
+
+var logger = new Logger("socialReport");
+
+/**
+ * Manage a message to home for the given beneficiary
+ *
+ * @constructor
+ */
+function SocialReports( beneficiaryID ) {
+	/**
+	 * the beneficiary ID
+	 */
+	this.subject = beneficiaryID;
+	
+	this.create = function() {
+		
+	};
+	
+	this.getLast = function() {
+		logger.trace('getLast');
+		var search = { subject: this.subject };
+		
+		return new promise( function(resolve, reject) {
+			physioDOM.db.collection("socialReport").find(search).sort({datetime: -1}).limit(1).toArray(function (err, doc) {
+				if (err) {
+					logger.alert("Error");
+					throw err;
+				}
+				logger.debug( doc.length );
+				if ( !doc[0]) {
+					reject({code: 404, error: "not found"});
+				} else {
+					resolve(doc[0]);
+				}
+			});
+		});
+	};
+	
+	this.getHistory = function( pg, offset ) {
+		
+	};
+}
+
+module.exports = SocialReports;
---
+++ server/lib/http/ISocialReport.js
@@ -0,0 +1,58 @@
+/**
+ * @file ISocialReport.js
+ * @module Http
+ */
+
+/* jslint node:true */
+/* global physioDOM */
+/* global -Promise */
+"use strict";
+
+
+var Logger = require("logger"),
+	ObjectID = require("mongodb").ObjectID,
+	Promise = require("rsvp").Promise,
+	moment = require("moment"),
+    SocialReport = require('../class/socialReport.js');
+var logger = new Logger("ISocialReport");
+
+/**
+ * function used to return errors
+ *
+ * @param err  error obkect
+ * @param res  response object
+ * @param next restify callback
+ */
+function errHandler( err, res, next ) {
+	if(err.stack) {
+		console.log(err.stack);
+	} else {
+		logger.alert(err);
+	}
+	res.send(err.code || 400, err);
+	next(false);
+}
+
+var ISocialReport = {
+	getLastReport : function(req, res, next) {
+		logger.trace("getLastReport");
+		physioDOM.Beneficiaries()
+			.then(function (beneficiaries) {
+				if (req.session.role === "beneficiary") {
+					return beneficiaries.getHHR(req.session.beneficiary);
+				} else {
+					return beneficiaries.getBeneficiaryByID(req.session, req.session.beneficiary);
+				}
+			})
+			.then( function( beneficiary ) {
+				return (new SocialReport( beneficiary._id)).getLast() ;
+			})
+			.then( function( resp ) {
+				res.send(resp);
+				next();
+			})
+			.catch( function(err) { errHandler(err, res, next); });
+	}
+};
+
+module.exports = ISocialReport;
