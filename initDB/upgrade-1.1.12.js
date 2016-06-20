85b8472f6cf9efc9791b64434d26f18c4e6853ff
fablec <fabrice.lecoz@telecomsante.com>
2016-06-20 13:41:01 +0200

    Fix upgrade script

--- initDB/upgrade-1.1.12.js
+++ initDB/upgrade-1.1.12.js
@@ -1,6 +1,6 @@
 /**
  * upgrade database for HHR-Pro v1.1.12
- * 
+ *
  * issue #301 : (Bug 636) Sunday was coded as 0 instead of 7
  */
 Math.sign = function sign(x) {
@@ -42,12 +42,12 @@ db.measurePlan.find({"frequency":"weekly"}).forEach(
 
 db.services.find({"frequency":"monthly"}).forEach(
 		function(item) {
-			var done = false; 
-			item.when.days.forEach( function(day, i) {
+			var done = false;
+			item.when.forEach( function(day, i) {
 				if(day % 10 === 0) {
-					item.when.days[i]+=Math.sign(day)*7;
-					done = true; 
-				} 
+					item.when[i]+=Math.sign(day)*7;
+					done = true;
+				}
 			} );
 			if(done) {
 				// printjson(item.when);
@@ -59,13 +59,13 @@ db.services.find({"frequency":"monthly"}).forEach(
 
 db.services.find({"frequency":"weekly"}).forEach(
 		function(item) {
-			var done = false; 
-			item.when.days.forEach( function(day, i) { 
-				if(day === 0) { 
-					item.when.days[i]+=7; 
-					done = true; 
-				} 
-			} ); 
+			var done = false;
+			item.when.forEach( function(day, i) {
+				if(day === 0) {
+					item.when[i]+=7;
+					done = true;
+				}
+			} );
 			if(done) {
 				// printjson(item.when);
 				db.services.save(item);
