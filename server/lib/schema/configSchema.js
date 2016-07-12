f6821dd7f8b6d79b2365fdbc3a8cb85bb600ef09
fablec <fabrice.lecoz@telecomsante.com>
2016-07-12 22:30:17 +0200

    Fix recurrent doPush

--- server/lib/schema/configSchema.js
+++ server/lib/schema/configSchema.js
@@ -1,71 +1,72 @@
 "use strict";
 
 var configSchema = {
-	id  : "/Config",
-	type : "object",
-	properties  : {
-		"port" : {type: "integer", minimum: 8000, maximum: 8080, required: true},
-		"cache": {type: "boolean", default: true},
-		"server": { 
-			type:"object", 
-			required: true,
-			properties  : {
-				protocol : { type:"string", required:true, description: "protocol http:// or https://" },
-				name     : { type:"string", required:true, description: "dns name of the service" }
-			}
-		},
-		"mongo": {
-			type               : "object",
-			properties         : {
-				ip  : {type: "string", description: "ip of the mongo server", required: true},
-				db  : {type: "string", description: "name of the database", required: true},
-				port: {
-					type       : "integer",
-					minimum    : 25000,
-					maximum    : 30000,
-					default    : 27017,
-					description: "port of the mongodb server if not default"
-				}
-			},
-			"additionalProperties": false
-		},
-		"Lang" : {
-			type       : "string",
-			enum       : ["en", "en_gb", "nl", "es", "fr"],
-			description: "default language of the instance",
-			required   : true
-		},
-		"languages": { type:"array", description:"array of all supported languages"},
-		"agenda": { type:"integer", description:"interval in minutes to send measures and symptoms plans"},
-		"key"  : {
-			type       : "string",
-			pattern    : "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$",
-			description: "key of the instance for the queue service"
-		},
-		"queue": {
-			type : "object",
-			properties : {
-				protocol: {type: "string", pattern: "^(http|https)", required: true},
-				ip      : {type: "string", required: true},
-				port    : {type: "integer", default: 9000, minimum: 9000, maximum: 9100},
-				duration : { type:"integer", default : 3, minimum: 2}
-			},
-			"additionalProperties": false
-		},
-		"IDS": {
-			type:"object",
-			properties: {
-				ip    :   {type: "string", required: true},
-				appName : {type: "string", required: true}
-			}
-		},
-		"country": { type:"string" },
-		"mailTpl": { type:"string" }
-	}
+  id        : "/Config",
+  type      : "object",
+  properties: {
+    "port"       : {type: "integer", minimum: 8000, maximum: 8080, required: true},
+    "cache"      : {type: "boolean", default: true},
+    "server"     : {
+      type      : "object",
+      required  : true,
+      properties: {
+        protocol: {type: "string", required: true, description: "protocol http:// or https://"},
+        name    : {type: "string", required: true, description: "dns name of the service"}
+      }
+    },
+    "mongo"      : {
+      type                  : "object",
+      properties            : {
+        ip  : {type: "string", description: "ip of the mongo server", required: true},
+        db  : {type: "string", description: "name of the database", required: true},
+        port: {
+          type       : "integer",
+          minimum    : 25000,
+          maximum    : 30000,
+          default    : 27017,
+          description: "port of the mongodb server if not default"
+        }
+      },
+      "additionalProperties": false
+    },
+    "Lang"       : {
+      type       : "string",
+      enum       : ["en", "en_gb", "nl", "es", "fr"],
+      description: "default language of the instance",
+      required   : true
+    },
+    "languages"  : {type: "array", description: "array of all supported languages"},
+    "agenda"     : {type: "string", description: "cron to send measures and symptoms plans"},
+    "agendaForce": {type: "string", description: "cron to send measures and symptoms plans forced"},
+    "key"        : {
+      type       : "string",
+      pattern    : "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$",
+      description: "key of the instance for the queue service"
+    },
+    "queue"      : {
+      type                  : "object",
+      properties            : {
+        protocol: {type: "string", pattern: "^(http|https)", required: true},
+        ip      : {type: "string", required: true},
+        port    : {type: "integer", default: 9000, minimum: 9000, maximum: 9100},
+        duration: {type: "integer", default: 3, minimum: 2}
+      },
+      "additionalProperties": false
+    },
+    "IDS"        : {
+      type      : "object",
+      properties: {
+        ip     : {type: "string", required: true},
+        appName: {type: "string", required: true}
+      }
+    },
+    "country"    : {type: "string"},
+    "mailTpl"    : {type: "string"}
+  }
 };
 
 var Validator = require('jsonschema').Validator;
 var validator = new Validator();
-validator.addSchema(configSchema,"/Config");
+validator.addSchema(configSchema, "/Config");
 
 module.exports.validator = validator;
